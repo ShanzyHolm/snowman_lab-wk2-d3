5b986c77be474283ac0689ec05fdbbc5feb0b721
Shannon Holmes <snholmes13@gmail.com>
2018-07-25 15:50:51 +0100

    add test guessed letters array starts empty

--- snowman_game.rb
+++ snowman_game.rb
@@ -2,8 +2,14 @@ class SnowmanGame
 
   # attr_reader
 
-  def initialize()
+  def initialize(player, word)
+    @player = player
+    @word = word
+    @guessed_letters = []
+  end
 
+  def guessed_letters()
+    return @guessed_letters.clone()
   end
 
 end
--- specs/snowman_game_spec.rb
+++ specs/snowman_game_spec.rb
@@ -6,8 +6,22 @@ require_relative("../snowman_game.rb")
 
 class SnowmanGameTest < MiniTest::Test
 
-def setup()
+  def setup()
+    @player = Player.new("Fred")
+    @word = HiddenWord.new("Wheel of Fortune")
+    @game = SnowmanGame.new(@player, @word)
+  end
 
-end
+  def test_player_name()
+    assert_equal("Fred", @player.name())
+  end
+
+  def test_display_word
+    assert_equal("***** ** *******", @word.display())
+  end
+
+  def test_guessed_letters_array_starts_empty()
+    assert_equal(0, @game.guessed_letters().count())
+  end
 
 end
